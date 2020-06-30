d5fbc44230ff17e26787670025944bc5ef471ba2
Kushal <student@pyb-8340-bchd.localdomain>
2020-06-30 17:52:18 -0400

    Looping techniques

---
+++ fact/looping02.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+with open("dnsservers.txt", "r") as dnsfile:
+
+#dnslist = dnsfile.readlines()
+
+
+    for svr in dnsfile:
+        svr = svr.rstrip('\n')
+
+        if svr.endswith('org'):
+            with open("org-domain.txt", "a") as srvfile:
+                srvfile.write(svr + "\n")
+
+        elif svr.endswith('com'):
+            with open("com-domain.txt", "a") as srvfile:
+                srvfile.write(svr + "\n")
+    
+    #print(svr, end="")
+
+
+#dnsfile.close()
+
+
